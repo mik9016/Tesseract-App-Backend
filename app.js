2f8e00c7d4c9893ac0a45038a9e5f7e472e15505
mik9016 <mikolajgruszecki@o2.pl>
2021-02-22 06:47:17 +0100

    change get route

--- app.js
+++ app.js
@@ -27,58 +27,55 @@ app.use(express.static("uploads"));
 //start app
 const port = process.env.PORT || 3000;
 console.log('strona dziala')
-// app.get('/cos', (req,res,next)=>{
-
-//   res.send(<h1>Witamy na stronie</h1>
-// })
-
-// app.post('/',async (req, res) => {
-//   try {
-//     if (!req.files) {
-//       res.send({
-//         status: false,
-//         message: "No file uploaded",
-//       });
-//     } else {
-//       //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
-//       let picture = req.files.picture;
-
-//       //Use the mv() method to place the file in upload directory (i.e. "uploads")
-//       picture.mv("./uploads/" + picture.name);
-
-//       //add filename to imgArr object
-//       Object.assign(imgArr, picture);
-//       // console.log(imgArr.name);
-
-//       //send response
-//       res.send({
-//         status: true,
-//         message: "File is uploaded",
-//         data: {
-//           name: picture.name,
-//           mimetype: picture.mimetype,
-//           size: picture.size,
-//         },
-//       });
-//     }
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
 
 
-// app.get('/', function (req, res, next) {
-//   try {
-//     res.sendFile(path.join(__dirname + "/uploads/" + imgArr.name)); //path to last file
-//     fileUpload.FileArray;
-//     setTimeout(() => {
-//       fs.unlink(path.join(__dirname + "/uploads/" + imgArr.name), () => {
-//         console.log("file removed");
-//       });
-//     },4000);
-//   } catch (err) {
-//     res.send(err);
-//   }
-// });
+app.post('*',async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
+      let picture = req.files.picture;
+
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      picture.mv("./uploads/" + picture.name);
+
+      //add filename to imgArr object
+      Object.assign(imgArr, picture);
+      // console.log(imgArr.name);
+
+      //send response
+      res.send({
+        status: true,
+        message: "File is uploaded",
+        data: {
+          name: picture.name,
+          mimetype: picture.mimetype,
+          size: picture.size,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+app.get('*', function (req, res, next) {
+  try {
+    res.sendFile(path.join(__dirname + "/uploads/" + imgArr.name)); //path to last file
+    fileUpload.FileArray;
+    setTimeout(() => {
+      fs.unlink(path.join(__dirname + "/uploads/" + imgArr.name), () => {
+        console.log("file removed");
+      });
+    },4000);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
