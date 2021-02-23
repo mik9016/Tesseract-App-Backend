6f3c2a2caaffb7412ad8c1316b93ac65dd8fe879
mik9016 <mikolajgruszecki@o2.pl>
2021-02-23 05:24:22 +0100

    check post

--- app.js
+++ app.js
@@ -26,39 +26,39 @@ const port = process.env.PORT || 3000;
 
 
 
-// app.post('/', async (req, res) => {
-//   try {
-//     if (!req.files) {
-//       res.send({
-//         status: false,
-//         message: "No file uploaded",
-//       });
-//     } else {
-//       //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
-//       let picture = req.files.picture;
+app.post('/', async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+      });
+    } else {
+      //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
+      let picture = req.files.picture;
 
-//       //Use the mv() method to place the file in upload directory (i.e. "uploads")
-//       picture.mv("./uploads/" + picture.name);
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      picture.mv("./uploads/" + picture.name);
 
-//       //add filename to imgArr object
-//       Object.assign(imgArr, picture);
-//       // console.log(imgArr.name);
+      //add filename to imgArr object
+      Object.assign(imgArr, picture);
+      // console.log(imgArr.name);
 
-//       //send response
-//       res.send({
-//         status: true,
-//         message: "File is uploaded",
-//         data: {
-//           name: picture.name,
-//           mimetype: picture.mimetype,
-//           size: picture.size,
-//         },
-//       });
-//     }
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+      //send response
+      res.send({
+        status: true,
+        message: "File is uploaded",
+        data: {
+          name: picture.name,
+          mimetype: picture.mimetype,
+          size: picture.size,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 
 // app.get('/', function (req, res, next) {
@@ -74,13 +74,13 @@ const port = process.env.PORT || 3000;
 //     res.send(err);
 //   }
 // });
-app.post('/',(req,res)=>{
-  res.send({status: 'ok',
-            message: 'message from POST req'
-        })
-})
-app.get('/', (req,res,next) =>{
-    res.send({status: 'OK',message:'message from GET req'});
-})
+// app.post('/',(req,res)=>{
+//   res.send({status: 'ok',
+//             message: 'message from POST req'
+//         })
+// })
+// app.get('/', (req,res,next) =>{
+//     res.send({status: 'OK',message:'message from GET req'});
+// })
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
