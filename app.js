19dbe1f6c51d69e36abb6a2d6732f7b652d08247
mik9016 <mikolajgruszecki@o2.pl>
2021-02-23 05:02:56 +0100

    serwer work check

--- app.js
+++ app.js
@@ -25,53 +25,59 @@ const port = process.env.PORT || 3000;
 console.log('strona dziala')
 
 
-app.post('/', async (req, res) => {
-  try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: "No file uploaded",
-      });
-    } else {
-      //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
-      let picture = req.files.picture;
+// app.post('/', async (req, res) => {
+//   try {
+//     if (!req.files) {
+//       res.send({
+//         status: false,
+//         message: "No file uploaded",
+//       });
+//     } else {
+//       //Use the name of the input field (i.e. "picture") to retrieve the uploaded file
+//       let picture = req.files.picture;
 
-      //Use the mv() method to place the file in upload directory (i.e. "uploads")
-      picture.mv("./uploads/" + picture.name);
+//       //Use the mv() method to place the file in upload directory (i.e. "uploads")
+//       picture.mv("./uploads/" + picture.name);
 
-      //add filename to imgArr object
-      Object.assign(imgArr, picture);
-      // console.log(imgArr.name);
+//       //add filename to imgArr object
+//       Object.assign(imgArr, picture);
+//       // console.log(imgArr.name);
 
-      //send response
-      res.send({
-        status: true,
-        message: "File is uploaded",
-        data: {
-          name: picture.name,
-          mimetype: picture.mimetype,
-          size: picture.size,
-        },
-      });
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+//       //send response
+//       res.send({
+//         status: true,
+//         message: "File is uploaded",
+//         data: {
+//           name: picture.name,
+//           mimetype: picture.mimetype,
+//           size: picture.size,
+//         },
+//       });
+//     }
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
 
 
-app.get('/', function (req, res, next) {
-  try {
-    res.sendFile(path.join(__dirname + "/uploads/" + imgArr.name)); //path to last file
-    fileUpload.FileArray;
-    setTimeout(() => {
-      fs.unlink(path.join(__dirname + "/uploads/" + imgArr.name), () => {
-        console.log("file removed");
-      });
-    },10000);
-  } catch (err) {
-    res.send(err);
-  }
-});
+// app.get('/', function (req, res, next) {
+//   try {
+//     res.sendFile(path.join(__dirname + "/uploads/" + imgArr.name)); //path to last file
+//     fileUpload.FileArray;
+//     setTimeout(() => {
+//       fs.unlink(path.join(__dirname + "/uploads/" + imgArr.name), () => {
+//         console.log("file removed");
+//       });
+//     },10000);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
+app.post('/',()=>{
+  res.send(console.log('odpowiedź z serwera POST req'))
+})
+app.get('/', (req,res,next) =>{
+    res.send(console.log('odpowiedź z serwera GET req'));
+})
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
